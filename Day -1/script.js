9194ae41999355efbc173f61a8ca95a4208ed95b
billoreankit66@gmail.com <billoreankit66@gmail.com>
2025-03-01 00:23:49 +0530

    Destructring

--- Day -1/script.js
+++ Day -1/script.js
@@ -12,3 +12,43 @@ console.log(typeof(a+b)); */
 /* Q3. converting string to number.
 let age = Number(prompt("what is your age :"));
 console.log(typeof(age)); */
+
+
+/* Q4 . swaping of two number. with using third variable 1st way
+let a = 10 ;
+let b = 20 ;
+let c;
+
+c = a;
+a = b;
+b = c;
+
+console.log(a);
+console.log(b); */
+
+
+ /* Q4. 2nd way without using third variable
+let a = 10;
+let b = 20;
+
+a = a+b // a= 30
+b = a-b // b= 10
+a = a-b // a= 20
+
+console.log(a);
+console.log(b); */
+
+
+
+/* Q4. 3rd way called de-structuring
+let a = 10;
+let b = 20;
+
+[a,b] = [b,a]
+
+console.log(a,b); */
+
+
+
+
+
