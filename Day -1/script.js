6599fa6d0fa93ecd1684e215796134f081b490b7
billoreankit66@gmail.com <billoreankit66@gmail.com>
2025-02-28 23:44:28 +0530

    Questions Update

--- Day -1/script.js
+++ Day -1/script.js
@@ -1,1 +1,14 @@
-alert("helo")
+/*Q1. Basic Addition .
+let a = 12;
+let b = 13;
+
+console.log(a+b); */
+
+/* Q2.Relationship between integer and string. 
+let a=10;
+let b ="20";
+console.log(typeof(a+b)); */
+
+/* Q3. converting string to number.
+let age = Number(prompt("what is your age :"));
+console.log(typeof(age)); */
